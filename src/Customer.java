8105147fe421f6e8f1be2457bffc87fde8c97093
Sethumrubeu <sethumgelaka6@gmail.com>
2024-11-26 22:16:25 +0530

    Updated Coutomer file

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,30 @@
-public class Customer {
+package CLI;
+
+public class Customer implements Runnable {
+    private final int customerId;
+    private final TicketPool ticketPool;
+    private final int retrievalRate;
+
+    public Customer(int customerId, TicketPool ticketPool, int retrievalRate) {
+        this.customerId = customerId;
+        this.ticketPool = ticketPool;
+        this.retrievalRate = retrievalRate;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!ticketPool.isTerminated()) {
+                Integer ticket = ticketPool.retrieveTicket();
+                if (ticket != null) {
+                    System.out.println("Customer " + customerId + " purchased Ticket ID: " + ticket);
+                } else {
+                    System.out.println("Customer " + customerId + ": No available tickets.");
+                }
+                Thread.sleep(retrievalRate); // Pause based on retrieval rate
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Customer " + customerId + " terminated.");
+        }
+    }
 }

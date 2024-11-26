fc12799292f8d6c06c1875e49e4c99f1ad7ebe36
Sethumrubeu <sethumgelaka6@gmail.com>
2024-11-26 22:16:00 +0530

    Updated Vendor file

--- src/Vendor.java
+++ src/Vendor.java
@@ -1,2 +1,29 @@
-public class Vendor {
+package CLI;
+
+public class Vendor implements Runnable {
+    private final int vendorId;
+    private final TicketPool ticketPool;
+    private final int releaseRate;
+    private static int ticketCounter = 1;
+
+    public Vendor(int vendorId, TicketPool ticketPool, int releaseRate) {
+        this.vendorId = vendorId;
+        this.ticketPool = ticketPool;
+        this.releaseRate = releaseRate;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!ticketPool.isTerminated()) {
+                synchronized (Vendor.class) {
+                    ticketPool.addTicket(ticketCounter++);
+                }
+                Thread.sleep(releaseRate); // Pause based on release rate
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Vendor " + vendorId + " terminated.");
+        }
+    }
 }
+

bf42258ae36c31ac633a26320a135fee2097b251
Sethumrubeu <sethumgelaka6@gmail.com>
2024-11-30 11:05:21 +0530

    Updated

--- src/CLI/TicketPool.java
+++ src/CLI/TicketPool.java
@@ -1,61 +1,23 @@
 package CLI;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TicketPool {
-    private final List<Integer> ticketList;
-    private final int maxTicketCapacity;
-    private boolean terminated = false;
+    private final Queue<Integer> tickets;
+    private final int totalTickets;
+    private final int maxCapacity;
+    private int ticketsAdded = 0; // Tracks the total number of tickets added to the pool
 
-    public TicketPool(int maxTicketCapacity) {
-        this.maxTicketCapacity = maxTicketCapacity;
-        this.ticketList = Collections.synchronizedList(new ArrayList<>());
+    public TicketPool(int totalTickets, int maxCapacity) {
+        this.totalTickets = totalTickets;
+        this.maxCapacity = maxCapacity;
+        this.tickets = new LinkedList<>();
     }
 
-    // Synchronized method to add tickets
-    public synchronized void addTicket(int ticketId) {
-        while (ticketList.size() >= maxTicketCapacity) {
-            try {
-                wait(); // Wait if the pool is at maximum capacity
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        ticketList.add(ticketId);
-        System.out.println("Vendor added Ticket ID: " + ticketId);
-        notifyAll(); // Notify customers that a new ticket is available
-    }
 
-    // Synchronized method to retrieve a ticket
-    public synchronized Integer retrieveTicket() {
-        while (ticketList.isEmpty() && !terminated) {
-            try {
-                wait(); // Wait if no tickets are available
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        if (!ticketList.isEmpty()) {
-            int ticketId = ticketList.remove(0);
-            System.out.println("Customer purchased Ticket ID: " + ticketId);
-            notifyAll(); // Notify vendors that space is available
-            return ticketId;
-        }
-        return null;
-    }
 
-    public synchronized void terminate() {
-        terminated = true;
-        notifyAll(); // Wake up all waiting threads to terminate
-    }
-
-    public synchronized boolean isTerminated() {
-        return terminated;
-    }
-
-    public synchronized int getAvailableTickets() {
-        return ticketList.size();
+    public synchronized boolean isFull() {
+        return ticketsAdded >= totalTickets;
     }
 }

e3ab29ba56cf20884376d02b0f38a3dd6608e42b
Sethumrubeu <sethumgelaka6@gmail.com>
2024-11-30 11:02:03 +0530

    Updated Configuration file

--- src/CLI/Configuration.java
+++ src/CLI/Configuration.java
@@ -1,26 +1,28 @@
 package CLI;
 
+import com.google.gson.Gson;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import com.google.gson.Gson;
 
 public class Configuration {
     private int totalTickets;
     private int ticketReleaseRate;
     private int customerRetrievalRate;
-    private int maxTicketCapacity;
-
+    private int maxTicketCapacity; // Add this field
 
     // Getters and Setters
     public int getTotalTickets() { return totalTickets; }
     public void setTotalTickets(int totalTickets) { this.totalTickets = totalTickets; }
+
     public int getTicketReleaseRate() { return ticketReleaseRate; }
     public void setTicketReleaseRate(int ticketReleaseRate) { this.ticketReleaseRate = ticketReleaseRate; }
+
     public int getCustomerRetrievalRate() { return customerRetrievalRate; }
     public void setCustomerRetrievalRate(int customerRetrievalRate) { this.customerRetrievalRate = customerRetrievalRate; }
-    public int getMaxTicketCapacity() { return maxTicketCapacity; }
-    public void setMaxTicketCapacity(int maxTicketCapacity) { this.maxTicketCapacity = maxTicketCapacity; }
+
+    public int getMaxTicketCapacity() { return maxTicketCapacity; } // Getter
+    public void setMaxTicketCapacity(int maxTicketCapacity) { this.maxTicketCapacity = maxTicketCapacity; } // Setter
 
     // Save configuration to a JSON file
     public void saveToFile(String fileName) {
